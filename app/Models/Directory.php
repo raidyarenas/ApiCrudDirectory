c66d688ee2b20427d69992139397642033649b5d
Raidy Arenas <raidy.arenas@gmail.com>
2021-02-05 15:02:09 -0400

    [ADD] added model directory

---
+++ app/Models/Directory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Directory extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'code',
+        'name',
+        'address',
+        'town',
+        'postalCode',
+        'city',
+        'phone',
+        'email',
+        'created_at',
+        'updated_at'
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+}
