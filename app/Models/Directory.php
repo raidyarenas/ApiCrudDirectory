7055bd1acb39d04089a18cc6773e410ff1c3abd5
Raidy Arenas <raidy.arenas@gmail.com>
2021-02-05 20:54:45 -0400

    Simple crud with filter directory

--- app/Models/Directory.php
+++ app/Models/Directory.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Directory extends Model
+class DirectoryModel extends Model
 {
     use HasFactory;
+
+    protected $table = 'directories';
 
     /**
      * The attributes that are mass assignable.
